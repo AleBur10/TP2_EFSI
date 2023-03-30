9965f3e966d936e5bd1f1990309aeacf611ee80a
ALEXIS BURSZTYN <46878021@ort>
2023-03-30 10:37:01 -0300

    Falta ver mas rapido

--- TP2_Perez_Bursztyn/script.js
+++ TP2_Perez_Bursztyn/script.js
@@ -2,20 +2,60 @@ let inputTarea = document.getElementById("tareas");
 let btnAgregarTarea = document.getElementById("botonAgregar")
 let ulListado = document.getElementById("lista")
 let tareasAgregadas = [
-        "Hacer matemática",
-        "Leer Lengua",
-        "Hacer Efsi",
+    {
+        texto: "Hacer matemática",
+        hecho: false,
+        fechaYHoraCreado: (new Date()).toLocaleDateString('en-US'),
+        fechaYHoraTachado: null,
+    },
+    {
+        texto: "Hacer Efsi",
+        hecho: false,
+        fechaYHoraCreado: (new Date()).toLocaleDateString('en-US'),
+        fechaYHoraTachado: null,
+    },
+    {
+        texto: "Leer Lengua",
+        hecho: true,
+        fechaYHoraCreado: (new Date()).toLocaleDateString('en-US'),
+        fechaYHoraTachado: null,
+    }
+
 ];
+
+
+
 const mostrarListado = () => {
     ulListado.innerHTML = ""; //borra los elementos de la lista
     tareasAgregadas.forEach(item => {
-    let li = document.createElement("li");
-    li.onclick = (e) => {
-        e.target.style.textDecoration = "line-through";
-    }
-    li.innerHTML = item; //agrega los elementos al html, en la lista
-    ulListado.appendChild(li); //agrega un elemento html a otro
-})
+        let li = document.createElement("li");
+        li.onclick = (e) => {
+            if(li.style.textDecoration != "line-through"){
+                li.style.textDecoration = "line-through";
+                item.hecho = true;
+                item.fechaYHoraTachado = (new Date()).toLocaleDateString('en-US');
+            }else{
+                li.style.textDecoration = "none";
+                item.hecho = false;
+                item.fechaYHoraTachado = null;
+            }            
+        }
+        if(item.hecho == true){
+            li.style.textDecoration = "line-through";
+            li.innerHTML = `
+                <input type="checkbox" checked name="" id="check"/> ${item.texto} ${item.fechaYHoraCreado}
+            `  
+        }else{
+            li.style.textDecoration = "none";
+            li.innerHTML = `
+                <input type="checkbox" name="" id="check"/> ${item.texto} ${item.fechaYHoraCreado}
+            `  
+        }
+                  
+        
+        //<input type="checkbox" onchange="CambiarEstado()" name="" id="check">' + item.texto + ' ' + item.fechaYHoraCreado; //agrega los elementos al html, en la lista
+        ulListado.appendChild(li); //agrega un elemento html a otro
+    })
 }
 
 mostrarListado();
@@ -23,17 +63,15 @@ mostrarListado();
 btnAgregarTarea.onclick = () => {
     if (inputTarea.value.length) {
         inputTarea.style.border = "1px solid black";
-        tareasAgregadas.push(inputTarea.value)
+        tareasAgregadas.push({texto:inputTarea.value, hecho:false,fechaYHoraCreado:(new Date()).toLocaleDateString('en-US'), fechaYHoraTachado:null});
         mostrarListado();
 
         inputTarea.value = "";
-    }else{
+    } else {
         alert("Ingresa una tarea valida")
         inputTarea.style.border = "1px solid red";
     }
 }
-
-
 
 
 /*let lista = [
