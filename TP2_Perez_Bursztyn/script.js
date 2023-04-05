4a50a0c4b8fb204d919015670c1c0a93090a78dd
MARTIN ARIEL PEREZ <47026770@ort>
2023-04-05 09:07:35 -0300

    falta ver mas rapido, hay foto

--- TP2_Perez_Bursztyn/script.js
+++ TP2_Perez_Bursztyn/script.js
@@ -1,28 +1,26 @@
 let inputTarea = document.getElementById("tareas");
-let btnAgregarTarea = document.getElementById("botonAgregar")
-let ulListado = document.getElementById("lista")
+let btnAgregarTarea = document.getElementById("botonAgregar");
+let btnMasRapido = document.getElementById("botonMasRapido");
+let ulListado = document.getElementById("lista");
+let listaTachados = [];
 let tareasAgregadas = [
     {
         texto: "Hacer matemática",
         hecho: false,
-        fechaYHoraCreado: (new Date()).toLocaleDateString('en-US'),
         fechaYHoraTachado: null,
     },
     {
         texto: "Hacer Efsi",
         hecho: false,
-        fechaYHoraCreado: (new Date()).toLocaleDateString('en-US'),
         fechaYHoraTachado: null,
     },
     {
         texto: "Leer Lengua",
-        hecho: true,
-        fechaYHoraCreado: (new Date()).toLocaleDateString('en-US'),
+        hecho: false,
         fechaYHoraTachado: null,
     }
 
 ];
-
 
 
 const mostrarListado = () => {
@@ -30,29 +28,34 @@ const mostrarListado = () => {
     tareasAgregadas.forEach(item => {
         let li = document.createElement("li");
         li.onclick = (e) => {
-            if(li.style.textDecoration != "line-through"){
+            if (li.style.textDecoration != "line-through") {
+                if (item.hecho === true) return;
                 li.style.textDecoration = "line-through";
                 item.hecho = true;
                 item.fechaYHoraTachado = (new Date()).toLocaleDateString('en-US');
-            }else{
+                listaTachados.push(item);
+                console.log(listaTachados);
+
+
+            } else {
                 li.style.textDecoration = "none";
                 item.hecho = false;
                 item.fechaYHoraTachado = null;
-            }            
+            }
         }
-        if(item.hecho == true){
+        if (item.hecho == true) {
             li.style.textDecoration = "line-through";
             li.innerHTML = `
-                <input type="checkbox" checked name="" id="check"/> ${item.texto} ${item.fechaYHoraCreado}
-            `  
-        }else{
+                <input type="checkbox" checked name="" id="check"/> ${item.texto}
+            `
+        } else {
             li.style.textDecoration = "none";
             li.innerHTML = `
-                <input type="checkbox" name="" id="check"/> ${item.texto} ${item.fechaYHoraCreado}
-            `  
+                <input type="checkbox" name="" id="check"/> ${item.texto}
+            `
         }
-                  
-        
+
+
         //<input type="checkbox" onchange="CambiarEstado()" name="" id="check">' + item.texto + ' ' + item.fechaYHoraCreado; //agrega los elementos al html, en la lista
         ulListado.appendChild(li); //agrega un elemento html a otro
     })
@@ -63,7 +66,7 @@ mostrarListado();
 btnAgregarTarea.onclick = () => {
     if (inputTarea.value.length) {
         inputTarea.style.border = "1px solid black";
-        tareasAgregadas.push({texto:inputTarea.value, hecho:false,fechaYHoraCreado:(new Date()).toLocaleDateString('en-US'), fechaYHoraTachado:null});
+        tareasAgregadas.push({ texto: inputTarea.value, hecho: false, fechaYHoraCreado: (new Date()).toLocaleDateString('en-US'), fechaYHoraTachado: null });
         mostrarListado();
 
         inputTarea.value = "";
@@ -71,6 +74,16 @@ btnAgregarTarea.onclick = () => {
         alert("Ingresa una tarea valida")
         inputTarea.style.border = "1px solid red";
     }
+}
+
+btnMasRapido.onclick = () => {
+    for (let i = 0; i < listaTachados.length; i++) {
+        if (listaTachados[i].hecho === true) {
+            alert("La tarea hecha en menor tiempo es " + listaTachados[i].texto);
+        }
+    }
+    return -1;
+
 }
 
 
